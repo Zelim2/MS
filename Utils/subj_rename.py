3d1c3e4ae53b3c1c4a2bdfb7705d09aa21094fd4
Alex Yolgin <alex.yolgin@mail.ru>
2023-01-21 14:19:39 +0300

    adding Utils folder

---
+++ Utils/subj_rename.py
@@ -0,0 +1,22 @@
+# libraries
+import json
+import pandas as pd
+
+def rename_subj(
+    df: pd.DataFrame,
+    column: str,
+    subj_rename_data: dict
+) -> pd.DataFrame:
+    # переименовывает регионы в соответствии с конфигом (приводит к общему стандарту)
+    # для унификации датафреймов и корректного отображения их на карте
+    while len(subj_rename_data) != 0:
+        for correct_name in subj_rename_data.keys():
+            for wrong_name in subj_rename_data[correct_name]:
+            #print(wrong_name)
+                if wrong_name in tuple(df[column]):
+                    df[column].replace(wrong_name, correct_name, inplace=True)
+                    break
+            subj_rename_data.pop(correct_name)
+            break
+    return df
+
